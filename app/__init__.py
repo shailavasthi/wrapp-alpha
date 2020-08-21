3c35665f26a97d4e47e6ad6f3f0c4b6892d7ecea
Shail Avasthi <shail.avasthi@yahoo.com>
2020-08-20 17:59:42 -0700

    heroku fix

--- app/__init__.py
+++ app/__init__.py
@@ -37,24 +37,24 @@ def create_app():
 	bootstrap.init_app(app)
 
 	if not app.debug and not app.testing:
-        # ...
+		# ...
 
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/wrapp.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+		if app.config['LOG_TO_STDOUT']:
+			stream_handler = logging.StreamHandler()
+			stream_handler.setLevel(logging.INFO)
+			app.logger.addHandler(stream_handler)
+		else:
+			if not os.path.exists('logs'):
+				os.mkdir('logs')
+			file_handler = RotatingFileHandler('logs/wrapp.log',
+											   maxBytes=10240, backupCount=10)
+			file_handler.setFormatter(logging.Formatter(
+				'%(asctime)s %(levelname)s: %(message)s '
+				'[in %(pathname)s:%(lineno)d]'))
+			file_handler.setLevel(logging.INFO)
+			app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Wrapp startup')
+		app.logger.setLevel(logging.INFO)
+		app.logger.info('Wrapp startup')
 
 	return app
